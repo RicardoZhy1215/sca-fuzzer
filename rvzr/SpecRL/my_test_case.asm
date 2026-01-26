c01f81292d1ec15f7bc6130ce85f6c943eb5b4a9
RicardoZhy1215 <hz25d@fsu.edu>
2026-01-26 16:18:04 -0500

    can run some training loops, still has some bugs to fix

--- rvzr/SpecRL/my_test_case.asm
+++ rvzr/SpecRL/my_test_case.asm
@@ -3,49 +3,52 @@
 .function_0:
 .bb_0.0:
 .macro.measurement_start: nop qword ptr [rax + 0xff]
+and rax, 0b1111111111111 # instrumentation
+sub al, byte ptr [r14 + rax] 
+verw dx 
+and rax, 0b1111111111111 # instrumentation
+neg dword ptr [r14 + rax] 
+pmovzxbd xmm6, xmm4 
+lea ebx, qword ptr [rcx] 
+cmovbe dx, bx 
+or eax, 0b1000000000000000000000000000000 # instrumentation
+bsr eax, eax 
+and rcx, 0b1111111111111 # instrumentation
+or byte ptr [r14 + rcx], cl 
+loope .bb_0.1 
+jmp .exit_0 
+.bb_0.1:
 cld  # instrumentation
-lea esi, qword ptr [rbx + rax] 
-lea rbx, qword ptr [rsi + rcx + 64085] 
+movq rsi, xmm5 
 and rdi, 0b1111111111111 # instrumentation
 add rdi, r14 # instrumentation
 and rcx, 0xff # instrumentation
 add rcx, 1 # instrumentation
-repe stosb  
+repne insd  
 sub rdi, r14 # instrumentation
-and rsi, 0b1111111111111 # instrumentation
-or dword ptr [r14 + rsi], -82 
-and rbx, 0b1111111110000 # instrumentation
-paddq xmm7, xmmword ptr [r14 + rbx] 
-pmovzxbq xmm7, xmm2 
-or dl, 0b1000 # instrumentation
-and dl, 0b11111000 # instrumentation
-and rdx, 0b1111111111111 # instrumentation
-sub byte ptr [r14 + rdx], -21 
 and rdi, 0b1111111111000 # instrumentation
-lock inc dword ptr [r14 + rdi] 
-cmp al, 127 
-packuswb xmm1, xmm6 
-and rsi, 0b1111111110000 # instrumentation
-psllq xmm4, xmmword ptr [r14 + rsi] 
-and rax, 0b1111111110000 # instrumentation
-paddusb xmm5, xmmword ptr [r14 + rax] 
+lock inc byte ptr [r14 + rdi] 
+movsx bx, cl 
+sub esi, edi 
+lea edi, qword ptr [rdx + rdx + 61683] 
+and rcx, 0b1111111111111 # instrumentation
+shl byte ptr [r14 + rcx], 135 
+stc  # instrumentation
+lea esi, qword ptr [rcx + rax + 43344] 
+or al, 67 
+out 212, al 
+and rcx, 0b1111111111000 # instrumentation
+lock add word ptr [r14 + rcx], dx 
+and rcx, 0b1111111111111 # instrumentation
+add al, byte ptr [r14 + rcx] 
 and rdx, 0b1111111111111 # instrumentation
-cmp word ptr [r14 + rdx], 19 
+nop qword ptr [r14 + rdx] 
+and rdi, 0b1111111111111 # instrumentation
+rcl word ptr [r14 + rdi], cl 
+stc  # instrumentation
 and rax, 0b1111111111000 # instrumentation
-lock add byte ptr [r14 + rax], 52 
-and rax, 0b1111111110000 # instrumentation
-pavgb xmm0, xmmword ptr [r14 + rax] 
-and rax, 0b1111111111111 # instrumentation
-movzx ebx, byte ptr [r14 + rax] 
-and rdi, 0b1111111110000 # instrumentation
-psubsb xmm3, xmmword ptr [r14 + rdi] 
-bswap rdx 
-sub ax, -12330 
-and rcx, 0b1111111110000 # instrumentation
-packusdw xmm5, xmmword ptr [r14 + rcx] 
-and rsi, 0b1111111111111 # instrumentation
-cvttss2si rcx, dword ptr [r14 + rsi] 
-sar si, 1 
+lock xor word ptr [r14 + rax], -81 
+rol ax, 1 
 stc  # instrumentation
 .exit_0:
 .macro.measurement_end: nop qword ptr [rax + 0xff]
